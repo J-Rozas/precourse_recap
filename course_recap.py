3a287a352812ebd6975c555faabb5e17bf43741a
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-03-28 16:25:29 +0100

    First commit

---
+++ course_recap.py
@@ -0,0 +1,20 @@
+# variables of type string
+my_string = "Hello, everyone"
+alt_string = "This is a test"
+
+# variables of type integer
+num1 = 10
+num2 = 28
+
+# use method upper() to capitalize a string
+my_string = my_string.upper()
+
+# concat two strings
+final_string = my_string + ". " + alt_string + "."
+
+# multiply two numbers
+result = num1 * num2
+
+# print variables
+print(final_string)
+print(result)
